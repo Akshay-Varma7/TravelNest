3c5601c156e3376f701d23bf4cb618b442884a67
Akshay-Varma7 <jampaniakshay@gmail.com>
2026-01-15 22:55:07 +0530

    crud

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const List = require("./models/list.js");
+const methodOverride = require("method-override");
+
+app.set("view engine","ejs");//s-1 setting viewing engine to ejs
+app.set("views",path.join(__dirname,"views"));//s-2 joining folder of views
+
+//middleware for parsing from url
+app.use(express.urlencoded({extended : true}));
+//middleware for method override
+app.use(methodOverride("_method"));
 
 // "./"-same folder & "../ "-go one level up folder - are relative paths
 
@@ -27,22 +36,75 @@ async function main(){
 app.get("/",(req,res)=>{
     res.send("working");
 });
-
-
+//index-route
 app.get("/list",async (req,res)=>{
-    let apollo = new List({//creating a document
-        title: "Apollo",
-        description: "24x7 services",
-        price: 0,
-        location: "Hyderabad",
-        country: "INDIA",
+    await List.find({}).then(allList=>{
+        res.render("list/index.ejs",{allList});//never do /list/index.ejs as "/" is absolute and not from views which should naturally happen
+    }).catch(err=>{
+        console.log(err);
     })
+});
+//to create new
+//s-1-get form
+app.get("/list/new",async (req,res)=>{// if below /list/:id then it first treats new as :id
+    res.render("list/new.ejs");
+});
+//s-2- post data
+app.post("/list",async (req,res)=>{
+    // let {title, description, image, price, location, country} = req.body;
+    //const list = {title,description,image,price,location,country
 
-    await apollo.save();//db operation // if model related collection dne then its created and save
-    //collection with model name + s
-    res.send("succesfully created")
+    let list = req.body.list;//imp
+    const newList = new List(list);//“Create a new MongoDB document using the List model and the data inside list.
+    await newList.save();
+    res.redirect("/list");//hitting route not just rendering an ejs
+});
+//read one user
+app.get("/list/:id",async (req,res)=>{
+    let {id} = req.params;
+    //variable exist in till this route hit and exists
+    const list = await List.findById(id);//document of that id 
+    res.render("list/show.ejs",{list});
+});
 
+//UPDATE
+//S-1-GET-show.ejs as for particular according to our design
+app.get("/list/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    const list = await List.findById(id);
+    res.render("list/edit.ejs",{list});
+});
+//S-2-PUT
+app.put("/list/:id",async (req,res)=>{
+    let {id} = req.params;
+    await List.findByIdAndUpdate(id,{...req.body.list});//***  (id,obj)//obj as {...req.body.list} i.e., deconstruct list object send by form
+    res.redirect(`/list/${id}`);
 })
+//DELETE
+app.delete("/list/:id",async (req,res)=>{
+    let {id} = req.params;
+    let deleted = await List.findByIdAndDelete(id);
+    console.log(deleted);
+    res.redirect("/list");//as response redirect
+})
+
+//redirect and render diff(like when to use)
+//we only send to ejs where it cant deconstruct like in express 
+
+// app.get("/list",async (req,res)=>{
+//     let apollo = new List({//creating a document
+//         title: "Apollo",
+//         description: "24x7 services",
+//         price: 0,
+//         location: "Hyderabad",
+//         country: "INDIA",
+//     })
+
+//     await apollo.save();//db operation // if model related collection dne then its created and save
+//     //collection with model name + s
+//     res.send("succesfully created")
+
+// })
 app.listen(port,()=>{
     console.log(`listening to port : ${port}`);
 });
